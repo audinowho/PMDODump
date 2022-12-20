a2301abdb26fdbfdff6f1d445e2cd25dddc5201b
Audino <2676737+audinowho@users.noreply.github.com>
2022-12-20 00:06:26 -0800

    merge and unlock

--- DataGenerator/Data/Zones/ZoneInfo.cs
+++ DataGenerator/Data/Zones/ZoneInfo.cs
@@ -733,7 +733,7 @@ namespace DataGenerator.Data
             {
                 #region FLYAWAY CLIFFS
                 {
-                    zone.Name = new LocalText("**Flyaway Cliffs");
+                    zone.Name = new LocalText("Flyaway Cliffs");
                     zone.Rescues = 2;
                     zone.Level = 20;
                     zone.Rogue = RogueStatus.ItemTransfer;
@@ -972,7 +972,7 @@ namespace DataGenerator.Data
             {
                 #region THUNDERSTRUCK PASS
                 {
-                    zone.Name = new LocalText("**Thunderstruck Pass");
+                    zone.Name = new LocalText("Thunderstruck Pass");
                     zone.Rescues = 2;
                     zone.Level = 30;
                     zone.Rogue = RogueStatus.ItemTransfer;
@@ -1095,7 +1095,7 @@ namespace DataGenerator.Data
             {
                 #region VEILED RIDGE
                 {
-                    zone.Name = new LocalText("**Veiled Ridge");
+                    zone.Name = new LocalText("Veiled Ridge");
                     zone.Rescues = 2;
                     zone.Level = 40;
                     zone.Rogue = RogueStatus.ItemTransfer;
@@ -1438,141 +1438,12 @@ namespace DataGenerator.Data
                 #endregion
             }
             else if (index == 8)
-            {
-
-                zone.Name = new LocalText("**Cave of Whispers");
-                zone.Level = 5;
-                zone.LevelCap = true;
-                zone.Rescues = 2;
-                zone.Rogue = RogueStatus.AllTransfer;
-
-                LayeredSegment floorSegment = new LayeredSegment();
-                floorSegment.IsRelevant = true;
-                floorSegment.ZoneSteps.Add(new SaveVarsZoneStep(PR_EXITS_RESCUE));
-                floorSegment.ZoneSteps.Add(new FloorNameIDZoneStep(PR_FLOOR_DATA, new LocalText("Cave of Whispers\nB{0}F")));
-
-                //money
-                MoneySpawnZoneStep moneySpawnZoneStep = new MoneySpawnZoneStep(PR_RESPAWN_MONEY, new RandRange(63, 72), new RandRange(21, 24));
-                moneySpawnZoneStep.ModStates.Add(new FlagType(typeof(CoinModGenState)));
-                floorSegment.ZoneSteps.Add(moneySpawnZoneStep);
-
-                //items!
-                ItemSpawnZoneStep itemSpawnZoneStep = new ItemSpawnZoneStep();
-                itemSpawnZoneStep.Priority = PR_RESPAWN_ITEM;
-                floorSegment.ZoneSteps.Add(itemSpawnZoneStep);
-
-                //mobs
-                TeamSpawnZoneStep poolSpawn = new TeamSpawnZoneStep();
-                poolSpawn.Priority = PR_RESPAWN_MOB;
-                //put something here
-                poolSpawn.TeamSizes.Add(1, new IntRange(0, 16), 12);
-                floorSegment.ZoneSteps.Add(poolSpawn);
-
-                TileSpawnZoneStep tileSpawn = new TileSpawnZoneStep();
-                tileSpawn.Priority = PR_RESPAWN_TRAP;
-                floorSegment.ZoneSteps.Add(tileSpawn);
-
-
-                for (int ii = 0; ii < 16; ii++)
-                {
-                    GridFloorGen layout = new GridFloorGen();
-
-
-                    //Floor settings
-                    if (ii < 8)
-                        AddFloorData(layout, "Star Cave.ogg", 3000, Map.SightRange.Clear, Map.SightRange.Dark);
-                    else if (ii < 12)
-                        AddFloorData(layout, "Limestone Cavern.ogg", 3000, Map.SightRange.Dark, Map.SightRange.Dark);
-                    else
-                        AddFloorData(layout, "Deep Limestone Cavern.ogg", 3000, Map.SightRange.Dark, Map.SightRange.Dark);
-
-                    //Tilesets
-                    if (ii < 2)
-                        AddTextureData(layout, "waterfall_pond_wall", "waterfall_pond_floor", "waterfall_pond_secondary", "rock");
-                    else if (ii < 4)
-                        AddTextureData(layout, "unused_waterfall_pond_wall", "unused_waterfall_pond_floor", "unused_waterfall_pond_secondary", "rock");
-                    else if (ii < 6)
-                        AddTextureData(layout, "crystal_cave_1_wall", "crystal_cave_1_floor", "crystal_cave_1_secondary", "rock");
-                    else if (ii < 8)
-                        AddTextureData(layout, "crystal_cave_2_wall", "crystal_cave_2_floor", "crystal_cave_2_secondary", "rock");
-                    else if (ii < 10)
-                        AddTextureData(layout, "lapis_cave_wall", "lapis_cave_floor", "lapis_cave_secondary", "rock");
-                    else if (ii < 12)
-                        AddTextureData(layout, "southern_cavern_2_wall", "southern_cavern_2_floor", "southern_cavern_2_secondary", "rock");
-                    else
-                        AddTextureData(layout, "wish_cave_2_wall", "wish_cave_2_floor", "wish_cave_2_secondary", "rock");
-
-                    //traps
-                    AddSingleTrapStep(layout, new RandRange(2, 4), "tile_wonder");//wonder tile
-                    AddTrapsSteps(layout, new RandRange(6, 9));
-
-                    //money - 9,280P to 25,536P
-                    if (ii < 8)
-                        AddMoneyData(layout, new RandRange(1, 5));
-                    else
-                        AddMoneyData(layout, new RandRange(3, 7));
-
-                    //1F Key guaranteed
-                    //SpecificItemZoneStep<MapGenContext> specificItemZoneStep = new SpecificItemZoneStep<MapGenContext>();
-                    //layout.ZoneSteps.Add(specificItemZoneStep);
-
-                    //enemies! ~ up to lv 20
-                    AddRespawnData(layout, 3, 80);
-
-                    //enemies
-                    AddEnemySpawnData(layout, 20, new RandRange(2, 4));
-
-
-                    //items
-                    if (ii < 8)
-                        AddItemData(layout, new RandRange(2, 6), 25);
-                    else
-                        AddItemData(layout, new RandRange(3, 6), 25);
-
-
-                    //construct paths
-                    {
-                        AddInitGridStep(layout, 4, 4, 10, 10);
-
-                        GridPathBranch<MapGenContext> path = new GridPathBranch<MapGenContext>();
-                        path.RoomComponents.Set(new ConnectivityRoom(ConnectivityRoom.Connectivity.Main));
-                        path.HallComponents.Set(new ConnectivityRoom(ConnectivityRoom.Connectivity.Main));
-                        path.RoomRatio = new RandRange(90);
-                        path.BranchRatio = new RandRange(0, 25);
-
-                        SpawnList<RoomGen<MapGenContext>> genericRooms = new SpawnList<RoomGen<MapGenContext>>();
-                        //cross
-                        genericRooms.Add(new RoomGenCross<MapGenContext>(new RandRange(4, 11), new RandRange(4, 11), new RandRange(2, 6), new RandRange(2, 6)), 10);
-                        //round
-                        genericRooms.Add(new RoomGenRound<MapGenContext>(new RandRange(5, 9), new RandRange(5, 9)), 10);
-                        path.GenericRooms = genericRooms;
-
-                        SpawnList<PermissiveRoomGen<MapGenContext>> genericHalls = new SpawnList<PermissiveRoomGen<MapGenContext>>();
-                        genericHalls.Add(new RoomGenAngledHall<MapGenContext>(50), 10);
-                        path.GenericHalls = genericHalls;
-
-                        layout.GenSteps.Add(PR_GRID_GEN, path);
-
-                        layout.GenSteps.Add(PR_GRID_GEN, CreateGenericConnect(75, 50));
-
-                    }
-
-                    AddDrawGridSteps(layout);
-
-                    AddStairStep(layout, false);
-
-                    AddWaterSteps(layout, "water", new RandRange(30));//water
-
-                    floorSegment.Floors.Add(layout);
-                }
-
-                zone.Segments.Add(floorSegment);
-            }
+                FillCaveOfWhispers(zone);
             else if (index == 9)
             {
                 #region MOONLIT COURTYARD
                 {
-                    zone.Name = new LocalText("**Moonlit Courtyard");
+                    zone.Name = new LocalText("Moonlit Courtyard");
                     zone.Rescues = 4;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -2079,7 +1950,7 @@ namespace DataGenerator.Data
             {
                 #region SLEEPING CALDERA
                 {
-                    zone.Name = new LocalText("**Sleeping Caldera");
+                    zone.Name = new LocalText("Sleeping Caldera");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -2237,7 +2108,7 @@ namespace DataGenerator.Data
             {
                 #region SHIMMER BAY
                 {
-                    zone.Name = new LocalText("**Shimmer Bay");
+                    zone.Name = new LocalText("Shimmer Bay");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
                     //zone.Persistent = true;
@@ -2391,7 +2262,7 @@ namespace DataGenerator.Data
             {
                 #region FERTILE RAVINE
                 {
-                    zone.Name = new LocalText("**Fertile Ravine");
+                    zone.Name = new LocalText("Fertile Ravine");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -2879,10 +2750,9 @@ namespace DataGenerator.Data
             {
                 #region FORSAKEN DESERT
                 {
-                    zone.Name = new LocalText("**Forsaken Desert");
+                    zone.Name = new LocalText("Forsaken Desert");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
-                    int max_floors = 4;
 
                     int max_floors = 4;
                     LayeredSegment floorSegment = new LayeredSegment();
@@ -3068,7 +2938,7 @@ namespace DataGenerator.Data
             {
                 #region SNOWBOUND PATH
                 {
-                    zone.Name = new LocalText("**Snowbound Path");
+                    zone.Name = new LocalText("Snowbound Path");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -3299,7 +3169,7 @@ namespace DataGenerator.Data
             {
                 #region RELIC TOWER
                 {
-                    zone.Name = new LocalText("**Relic Tower");
+                    zone.Name = new LocalText("Relic Tower");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
                     int max_floors = 10;
@@ -3427,7 +3297,7 @@ namespace DataGenerator.Data
             {
                 #region OVERGROWN WILDS
                 {
-                    zone.Name = new LocalText("**Overgrown Wilds");
+                    zone.Name = new LocalText("Overgrown Wilds");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -3766,7 +3636,7 @@ namespace DataGenerator.Data
             {
                 #region TRICKSTER STEPPE
                 {
-                    zone.Name = new LocalText("**Trickster Steppe");
+                    zone.Name = new LocalText("Trickster Steppe");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -3992,7 +3862,7 @@ namespace DataGenerator.Data
             {
                 #region LAVA FLOE ISLAND
                 {
-                    zone.Name = new LocalText("**Lava Floe Island");
+                    zone.Name = new LocalText("Lava Floe Island");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
@@ -4228,7 +4098,7 @@ namespace DataGenerator.Data
             {
                 #region FISSURE QUARRY
                 {
-                    zone.Name = new LocalText("**Fissure Quarry");
+                    zone.Name = new LocalText("Fissure Quarry");
                     zone.Rescues = 2;
                     zone.Rogue = RogueStatus.ItemTransfer;
 
